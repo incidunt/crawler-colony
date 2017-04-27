7b79b50876ece454d6212929a440544ccae13052
dang <dangfugui@163.com>
2017-04-28 00:14:15 +0800

    crawler test

--- crawler-core/src/main/java/com/dang/crawler/core/tool/Mongo.java
+++ crawler-core/src/main/java/com/dang/crawler/core/tool/Mongo.java
@@ -2,13 +2,17 @@ package com.dang.crawler.core.tool;
 
 import com.dang.colony.resources.mongodb.MongoDB;
 import com.mongodb.DBObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by duang on 2017/4/25.
  */
 public class Mongo {
+    private static  Logger log = LoggerFactory.getLogger(Mongo.class);
     public static void main(String []args){
         DBObject ob = MongoDB.find("test", "name", "test1");
         System.out.println(ob.get("name"));
+        log.info("end");
     }
 }
